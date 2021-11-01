39762348068fb1063c950a234a60644fff676d59
Kiernan2 <kiernan1994@gmail.com>
2021-11-01 13:47:05 -0700

    Change to support promises

--- src/main.js
+++ src/main.js
@@ -2,14 +2,37 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js';
 
 function Fahrenheit_Converter(kelvin) {
   return ((kelvin - 273.15) * 9/5 + 32);
 }
 
+function clearFields() {
+  $('#location').val("");
+  $('#latition').val("");
+  $('#longitude').val("");
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+}
+
+
 $(document).ready(function() {
-  
   let mode = "city";
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    clearFields();
+    let promise = WeatherService.getWeather(city);
+    console.log(promise)
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${Fahrenheit_Converter(body.main.temp)} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
 
   $('#radioInput').click(function() {
     if ($('#city').hasClass('hidden')) {
@@ -21,60 +44,5 @@ $(document).ready(function() {
       $('#city').addClass('hidden');
       mode = "lat-lon";
     }
-  });
-
-  $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    const lat = $('#latitude').val();
-    const lon = $('#longitude').val();
-    $('#location').val("");
-    $('#latitude').val("");
-    $('#longitude').val("");
-
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = '';
-      if (mode === 'city') {
-        url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-      }
-      else if (mode === 'lat-lon') {
-        url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`;
-      }
-
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
-        }
-      };
-
-      // request.onreadystatechange = function() {
-      //   if (this.readyState === 4 && this.status === 200) {
-      //     const response = JSON.parse(this.responseText);
-      //     getElements(response);
-      //   }
-      // };
-
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${response.name} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${Fahrenheit_Converter(body.main.temp)} degrees.`);
-      $('.showError').text("");
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`);
-      $('.showHumidity').text("");
-      $('.showTemp').text("");
-    });
-
-    // function getElements(response) {
-    //   $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
-    //   $('.showTemp').text(`The temperature in Fahrenheit is ${Fahrenheit_Converter(response.main.temp)} Degrees.`);
-    // }
-
   });
 });
