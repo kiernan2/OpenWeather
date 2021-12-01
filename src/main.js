1ba2231cfcd3b7369c66acd3dc5265675a686690
Kiernan2 <kiernan1994@gmail.com>
2021-12-01 14:30:11 -0800

    Changed to match 2.3.3.2

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import WeatherService from './weather-service.js';
+import WeatherService from './services/weather-service.js';
+import GiphyService from './services/giphy-service.js';
 
 function Fahrenheit_Converter(kelvin) {
   return ((kelvin - 273.15) * 9/5 + 32);
@@ -15,6 +16,19 @@ function clearFields() {
   $('.showErrors').text("");
   $('.showHumidity').text("");
   $('.showTemp').text("");
+}
+
+function displayWeatherDescription(description) {
+  $('.weather-description').text(`The weather is ${description}`)
+}
+
+function displayGif(response) {
+  const url = response.data[0].images.downsized.url
+  $('.show-gif').html(`<img src='${url}'>`);
+}
+
+function displayError(error) {
+  $('.show-error').text(`${error}`);
 }
 
 function getElements(response) {
@@ -35,6 +49,23 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     clearFields();
-    makeApiCall(city);
+    WeatherService.getWeather(city)
+    .then(function(weatherResponse) {
+      if (weatherResponse instanceof Error) {
+        throw Error(`OpenWeather API error: ${weatherResponse.message}`);
+      }
+      const weatherDescription = weatherResponse.message[0].description;
+      displayWeatherDescription(weatherDescription);
+      return GiphyService.getGif(weatherDescription);
+    })
+    .then(function(giphyResponse) {
+      if (giphyResponse instanceof Error) {
+        throw Error(`Giphy API error: ${giphyResponse.message}`)
+      }
+      displayGif(giphyResponse);
+    })
+    .catch(function(error) {
+      displayError(error.message)
+    })
   });
 });
--- src/services/weather-service.js
+++ src/services/weather-service.js
@@ -1,13 +1,14 @@
 export default class WeatherService {
   static async getWeather(city) {
-    try {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}`)
+    .then(function(response) {
       if (!response.ok) {
         throw Error(response.statusText);
       }
       return response.json();
-    } catch(error) {
-      return error.message;
-    }
+    })
+    .catch(function(error) {
+      return Error(error);
+    })
   }
 }
